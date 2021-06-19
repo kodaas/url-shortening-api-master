4de44f008aa2c5fb516d2cc84ec32e211ad1e666
Ajala Fiyinfoluwa John <johnajala204@gmail.com>
2021-06-19 14:34:15 +0100

    Added fetch link func and get input func

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ function closeNav() {
     overlay.style.display = "none";
     body.style.overflowY = 'auto'
 }
+
+async function getShortLink(link) {
+
+    let URI = `https://api.shrtco.de/v2/shorten?url=${link}`
+
+    let res = await fetch(URI,);
+
+    if (!res.ok) {
+        throw new Error("cannot fetch data");
+    }
+    let data = await res.json();
+
+    return data;
+
+}
+
+function getFormInput() {
+    let form = document.querySelector('form')
+    let label = document.querySelector('label')
+    let link = form.link
+
+    if (!link.value || !link.value.include('://') || !link.value.include('.')) {
+        label.style.display = "block"
+        link.classList.add('bad')
+    } else {
+        getShortLink(link.value).then((data) => {
+            console.log(data)
+        })
+    }
+}
